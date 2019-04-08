cfcb48ec3af6e507c4c3dde1fc86121906c29661
kammic94 <kamil.michalik.94@gmail.com>
2019-04-08 12:48:14 +0200

    Add files via upload

---
+++ www/js/pgb.js
@@ -0,0 +1,19 @@
+function init() {
+	
+document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+function showInfo() {
+	info = 'Hello, this is my businesscard + '\n' +
+			'=====' + '\n' +
+			'Name and surname: Kamil Michalik' + '\n' +
+			'Email : kamil.michalik.94@gmail.com' + '\n' +
+			'Den group number: 187439 ' + '\n' +
+			'Field of study: Applied Computer Science' + '\n' +	
+			navigator.notification.alert(info);
+
+}
